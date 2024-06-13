ebfcb4cd785310ee240a91ee2b51740bdae003f1
Jacky Puso Lephoto <jacky.lephoto@gmail.com>
2024-06-13 20:10:57 +0200

    add navbar

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import './Main.css';
+import "./Main.css";
 
 const Main = () => {
   return (
-    <div className="container">
-      {/* <Navbar /> */}
-      <Outlet />
-      {/* <Footer /> */}
+    <div>
+      <Navbar />
+      <div className="container">
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   );
 };
